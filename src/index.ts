a6f06b7e0fe23e9559ef7ec34861c20b8646803b
maksim.skvortsov <maksskvor@yandex.ru>
2025-07-16 14:30:48 +0300

    nest

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { app } from './app';
-import { NodeRuntime } from '@effect/platform-node';
-import { NodeServer } from 'effect-http-node';
-
-function bootstrap() {
-  app.pipe(NodeServer.listen({ port: 3000 }), NodeRuntime.runMain);
-}
-
-bootstrap();
